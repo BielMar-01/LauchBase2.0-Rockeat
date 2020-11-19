9901b7a25db3ef24d697e55bd84a42c657265ff3
Gabriel <bielmarques922@gmail.com>
2020-11-19 07:52:11 -0300

    Estilizando os Videos

--- Semana01/modulo02/scripts.js
+++ Semana01/modulo02/scripts.js
@@ -6,11 +6,19 @@ const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {
   card.addEventListener("click", function() {
-    modalOverlay.classList.add('active');
+    //VIDEO
+    const videoId = card.getAttribute("id")
+    //MODAL
+    modalOverlay.classList.add('active')
+    //IFRAME
+    modalOverlay.querySelector(".iframe").src = `https://www.youtube.com/embed/${videoId}`;
   })
 }
 // FECHANDO O MODAL
 document.querySelector('.close_modal').addEventListener("click", function() {
-  modalOverlay.classList.remove("active");
+  modalOverlay.classList.remove("active")
+  modalOverlay.querySelector(".iframe").src = "";
 });
+
+
 
