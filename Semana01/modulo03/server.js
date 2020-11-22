4d147fc4fa419570b4ba57ca4e0a2f1bec1da361
Gabriel <bielmarques922@gmail.com>
2020-11-22 19:02:09 -0300

    Configurando Template Engine

--- Semana01/modulo03/server.js
+++ Semana01/modulo03/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const nunjuncks = require('nunjuncks');
+
 const server = express();
+
+server.set("view engine", "html");
+
+nunjuncks.configure("views", {
+  express:server
+})
 
 server.get("/", function(req, res) {
   return res.send("Hi! How's going?");
