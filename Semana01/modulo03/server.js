bb66f34826ebcc24b0a55f6528e3d25ccd323134
Gabriel <bielmarques922@gmail.com>
2020-11-22 21:35:27 -0300

    Trazendo o front para o Servidor

--- Semana01/modulo03/server.js
+++ Semana01/modulo03/server.js
@@ -1,16 +1,22 @@
 const express = require('express');
-const nunjuncks = require('nunjuncks');
+const nunjucks = require('nunjucks');
 
 const server = express();
 
+server.use(express.static('public'));
+
 server.set("view engine", "html");
 
-nunjuncks.configure("views", {
-  express:server
+nunjucks.configure("views", {
+  express: server
 })
 
 server.get("/", function(req, res) {
-  return res.send("Hi! How's going?");
+  return res.render("index")
+});
+
+server.get("/portfolio", function(req, res) {
+  return res.render("portfolio")
 });
 
 server.listen(5000, function() {
