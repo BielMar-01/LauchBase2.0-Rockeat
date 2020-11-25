ab20ff0dc1c360c5831e0a0a2bd3be293f569e1a
Gabriel <bielmarques922@gmail.com>
2020-11-25 08:38:10 -0300

    Exportanto e Importanto Js

---
+++ Semana01/modulo03/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: "-ZV-_7vNRGw",
+    title: "Formulario Animado com JS puro e CSS Animation | Mayk Brito",
+    duration: "57 min",
+    price: "Free"
+  },
+  {
+    id: "XdqD8qi44Cg",
+    title: "Landing page to pinterest com o Mayk Brito curso de JS puro para iniciantes",
+    duration: "25 min",
+    price: "Free"
+  },
+  {
+    id: "-ZV-_7vNRGw",
+    title: "Formulario Animado com JS puro e CSS Animation | Mayk Brito",
+    duration: "57 min",
+    price: "Free"
+  },
+  {
+    id: "-ZV-_7vNRGw",
+    title: "Formulario Animado com JS puro e CSS Animation | Mayk Brito",
+    duration: "57 min",
+    price: "Free"
+  },
+  {
+    id: "-ZV-_7vNRGw",
+    title: "Formulario Animado com JS puro e CSS Animation | Mayk Brito",
+    duration: "57 min",
+    price: "Free"
+  },
+  {
+    id: "-ZV-_7vNRGw",
+    title: "Formulario Animado com JS puro e CSS Animation | Mayk Brito",
+    duration: "57 min",
+    price: "Free"
+  },
+]
