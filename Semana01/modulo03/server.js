333b8ccdce8aa459ee40048e76fc7a2f97f7018d
Gabriel <bielmarques922@gmail.com>
2020-11-26 09:05:21 -0300

    Melhorando pagina sobre

--- Semana01/modulo03/server.js
+++ Semana01/modulo03/server.js
@@ -9,11 +9,24 @@ server.use(express.static('public'));
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-  express: server
+  express: server,
+  autoescape: false
 })
 
 server.get("/", function(req, res) {
-  return res.render("about")
+  const about = {
+    avatar_url: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+    name: "Mayk Brito",
+    role: "Instrutor - Rocketseat",
+    description: 'Programador - Fullstack que trabalha na <a href="https://rocketseat.com.br" target="_black">Rocketseat</a> Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    links: [
+      { name: "Github", url: "http://github.com/maykbrito/" },  
+      { name: "Twitter", url: "http://twitter.com/maykbrito/" },  
+      { name: "Linkedin", url: "http://www.linkedin.com/in/maykbrito" }
+    ]
+  }
+
+  return res.render("about", { about: about })
 });
 
 server.get("/portfolio", function(req, res) {
