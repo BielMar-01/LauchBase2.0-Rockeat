5fb35acc5e06174f0d6a8ca836aa6f0004416d46
Gabriel <bielmarques922@gmail.com>
2020-12-03 08:01:52 -0300

    Redirecionando URL com JS

--- Semana01/modulo03/public/scripts.js
+++ Semana01/modulo03/public/scripts.js
@@ -7,18 +7,14 @@ const cards = document.querySelectorAll('.card');
 for (let card of cards) {
   card.addEventListener("click", function() {
     //VIDEO
-    const videoId = card.getAttribute("id")
+    const videoId = card.getAttribute("id");
+    window.location.href = `/video?id=${videoId}`;
     //MODAL
-    modalOverlay.classList.add('active')
+    modalOverlay.classList.add('active');
     //IFRAME
     modalOverlay.querySelector(".iframe").src = `https://www.youtube.com/embed/${videoId}`;
   })
 }
-// FECHANDO O MODAL
-document.querySelector('.close_modal').addEventListener("click", function() {
-  modalOverlay.classList.remove("active")
-  modalOverlay.querySelector(".iframe").src = "";
-});
 
 
 
